41ba35281e5aed492d9d0d92322faa6546fceaca
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2019-04-18 16:18:18 -0500

    init

---
+++ format-bitrate/formatbitrate.py
@@ -0,0 +1,10 @@
+from gi.repository import GObject, RB, Peas
+
+class FormatBitratePlugin (GObject.Object, Peas.Activatable):
+    object = GObject.property(type=GObject.Object)
+
+    def __init__(self):
+        super(FormatBitratePlugin, self).__init__()
+
+    def do_activate(self):
+        print("Hello World")
